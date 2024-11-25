edeca398a6d653c9b36d2542ec2e19e5b84e73e7
macij1 <124784259+macij1@users.noreply.github.com>
2024-11-25 17:59:25 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,65 +1,74 @@
 #!/usr/bin/env python3
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, LSTM, Bidirectional, Embedding
+from tensorflow.keras.models import Sequential
 
-from keras.layers import Dense, LSTM, Bidirectional, Merge
-from keras.layers.embeddings import Embedding
-from keras.models import Sequential
-
-
-def get_LSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_LSTM(input_dim, output_dim, max_length, no_activities):
     model = Sequential(name='LSTM')
-    model.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model.add(LSTM(output_dim))
-    model.add(Dense(no_activities, activation='softmax'))
+    model.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_length, mask_zero=True))
+    model.add(LSTM(units=output_dim))
+    model.add(Dense(units=no_activities, activation='softmax'))
     return model
 
-
-def get_biLSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_biLSTM(input_dim, output_dim, max_length, no_activities):
     model = Sequential(name='biLSTM')
-    model.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model.add(Bidirectional(LSTM(output_dim)))
-    model.add(Dense(no_activities, activation='softmax'))
+    model.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_length, mask_zero=True))
+    model.add(Bidirectional(LSTM(units=output_dim)))
+    model.add(Dense(units=no_activities, activation='softmax'))
     return model
 
-
-def get_Ensemble2LSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_Ensemble2LSTM(input_dim, output_dim, max_length, no_activities):
+    # In Keras 3.0, the Merge layer is replaced with functional API or concatenation
     model1 = Sequential()
-    model1.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model1.add(Bidirectional(LSTM(output_dim)))
-
+    model1.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_length, mask_zero=True))
+    model1.add(Bidirectional(LSTM(units=output_dim)))
+    
     model2 = Sequential()
-    model2.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model2.add(LSTM(output_dim))
-
-    model = Sequential(name='Ensemble2LSTM')
-    model.add(Merge([model1, model2], mode='concat'))
-    model.add(Dense(no_activities, activation='softmax'))
+    model2.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_length, mask_zero=True))
+    model2.add(LSTM(units=output_dim))
+    
+    # Use functional API for merging
+    inputs1 = model1.input
+    outputs1 = model1.output
+    inputs2 = model2.input
+    outputs2 = model2.output
+    
+    merged = keras.layers.Concatenate()([outputs1, outputs2])
+    output = Dense(units=no_activities, activation='softmax')(merged)
+    
+    model = keras.Model(inputs=[inputs1, inputs2], outputs=output, name='Ensemble2LSTM')
     return model
 
-
-def get_CascadeEnsembleLSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_CascadeEnsembleLSTM(input_dim, output_dim, max_length, no_activities):
+    # Similar to Ensemble2LSTM, use functional API
     model1 = Sequential()
-    model1.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model1.add(Bidirectional(LSTM(output_dim, return_sequences=True)))
-
+    model1.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_length, mask_zero=True))
+    model1.add(Bidirectional(LSTM(units=output_dim, return_sequences=True)))
+    
     model2 = Sequential()
-    model2.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model2.add(LSTM(output_dim, return_sequences=True))
-
-    model = Sequential(name='CascadeEnsembleLSTM')
-    model.add(Merge([model1, model2], mode='concat'))
-    model.add(LSTM(output_dim))
-    model.add(Dense(no_activities, activation='softmax'))
+    model2.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_length, mask_zero=True))
+    model2.add(LSTM(units=output_dim, return_sequences=True))
+    
+    inputs1 = model1.input
+    outputs1 = model1.output
+    inputs2 = model2.input
+    outputs2 = model2.output
+    
+    merged = keras.layers.Concatenate()([outputs1, outputs2])
+    x = LSTM(units=output_dim)(merged)
+    output = Dense(units=no_activities, activation='softmax')(x)
+    
+    model = keras.Model(inputs=[inputs1, inputs2], outputs=output, name='CascadeEnsembleLSTM')
     return model
 
-
-def get_CascadeLSTM(input_dim, output_dim, max_lenght, no_activities):
+def get_CascadeLSTM(input_dim, output_dim, max_length, no_activities):
     model = Sequential(name='CascadeLSTM')
-    model.add(Embedding(input_dim, output_dim, input_length=max_lenght, mask_zero=True))
-    model.add(Bidirectional(LSTM(output_dim, return_sequences=True)))
-    model.add(LSTM(output_dim))
-    model.add(Dense(no_activities, activation='softmax'))
+    model.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=max_length, mask_zero=True))
+    model.add(Bidirectional(LSTM(units=output_dim, return_sequences=True)))
+    model.add(LSTM(units=output_dim))
+    model.add(Dense(units=no_activities, activation='softmax'))
     return model
-
 
 def compileModel(model):
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
